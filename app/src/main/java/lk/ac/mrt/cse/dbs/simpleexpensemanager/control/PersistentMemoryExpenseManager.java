49f8f0ceaba6b1a583d3729a1179fdce1057071b
navindu-ds <navindu.20@cse.mrt.ac.lk>
2022-12-12 10:01:04 +0530

    Debug fixes

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentMemoryExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentMemoryExpenseManager.java
@@ -33,9 +33,9 @@ public class PersistentMemoryExpenseManager extends ExpenseManager {
         setAccountsDAO(persistentAccountDAO);
 
         // dummy data
-        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
-        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
-        getAccountsDAO().addAccount(dummyAcct1);
-        getAccountsDAO().addAccount(dummyAcct2);
+//        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+//        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+//        getAccountsDAO().addAccount(dummyAcct1);
+//        getAccountsDAO().addAccount(dummyAcct2);
     }
 }
